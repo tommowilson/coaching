35f6a1c6938e6b2467e36347328da6bacd42ce54
Tom Wilson <tommo.wilson@gmail.com>
2025-11-02 10:11:28 +1300

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other entry modules.
+(() => {
+
+})();
+
+// This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
+(() => {
+"use strict";
+// extracted by mini-css-extract-plugin
+
+})();
+
+/******/ })()
+;
